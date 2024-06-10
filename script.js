5a84ad2ff4791ae3f7a685d37ec96b36ee3b8719
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2024-06-10 23:31:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function binarySearch(nums, target) {
 // Your code here
 // Return index if target is present
 // else return -1
+	let left = 0;
+	let right = nums.length;
+	while(left <= right) {
+		let mid = Math.floor((left+right)/2);
+		if(nums[mid] === target) return mid;
+		else if(nums[mid] > target) right = mid;
+		else left = mid;
+	}
+	return -1;
 }
 
 module.exports = binarySearch;
